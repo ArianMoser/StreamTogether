7c114c10d7ca56cda96a8cbcca5cdefd61091035
ArianMoser <arian_moser@yahoo.de>
2018-04-15 14:43:43 +0200

    Updated sql scheme

--- database/streamtogether.sql
+++ database/streamtogether.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 14. Apr 2018 um 00:50
--- Server-Version: 10.1.31-MariaDB
--- PHP-Version: 5.6.34
+-- Generation Time: Apr 15, 2018 at 02:37 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,81 +19,114 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `streamtogether`
+-- Database: `streamtogether`
 --
-CREATE DATABASE IF NOT EXISTS `streamtogether` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `streamtogether` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `streamtogether`;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `playlist`
+-- Table structure for table `playlist`
 --
 
 CREATE TABLE `playlist` (
-  `roomID` int(11) NOT NULL,
-  `videoID` int(11) NOT NULL
+  `room_ID` int(11) NOT NULL,
+  `video_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `playlist`
+--
+
+INSERT INTO `playlist` (`room_ID`, `video_ID`) VALUES
+(1, 1),
+(1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `room`
+-- Table structure for table `room`
 --
 
 CREATE TABLE `room` (
   `ID` int(11) NOT NULL,
-  `title` varchar(500) DEFAULT NULL,
-  `description` varchar(500) DEFAULT NULL
+  `title` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `thumbnail` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'room_default.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`ID`, `title`, `description`, `password`, `thumbnail`) VALUES
+(1, 'Test-title', 'test-description', 'test-password', 'room_default.png');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `ID` int(11) NOT NULL,
-  `username` varchar(500) NOT NULL,
-  `email` varchar(500) NOT NULL,
-  `password` varchar(500) NOT NULL,
-  `room-id` int(11) DEFAULT NULL
+  `username` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `active` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for all users ';
 
 --
--- Daten für Tabelle `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`ID`, `username`, `email`, `password`, `room-id`) VALUES
-(1, 'Alex', 'test@web.de', 'aidwhaäjdawidh', NULL);
+INSERT INTO `user` (`ID`, `username`, `email`, `password`, `room_id`, `active`) VALUES
+(1, 'Alex', 'test@web.de', 'aidwha?jdawidh', NULL, 0),
+(2, 'test', 'test', '$2a$11$vfbVtZJTu939uig4I10HveA/fL.zOmk1U5wTW/k9wPx3ZEPtT03t.', 1, 0),
+(3, 'test123', 'test123', '$2a$11$ITRzdbT0FlLK9qcvF1uYceAo9iDgEKp9FD8/PVrI4aKfXErkUjkai', NULL, 0),
+(4, '1', '1', '$2a$11$QCyVV.WEBgm9qmV11vaN3eQ6tu0pAaH1nRVqn7PXgQHXdNiwPdN5G', NULL, 0),
+(5, '2', '2', '$2a$11$KdvYVEGu/sDOSKUekKBFb.Mgmb8SicvJtpfqD8Z9zTugKKVb0XgoS', NULL, 0),
+(6, '12', '12', '$2a$11$EQAnKaSQYwaFhmzw1qioze97S/g6mS22.AoFZNn7j.TQbpFiuvd.K', NULL, 0),
+(7, '13', '13', '$2a$11$OvRuUUza9I/VV35Ircr6cuKqrUfWtFkf4TS29hon/Kj1dpzZiLElW', NULL, 0);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `video`
+-- Table structure for table `video`
 --
 
 CREATE TABLE `video` (
   `ID` int(11) NOT NULL,
-  `title` varchar(500) DEFAULT NULL,
-  `description` varchar(500) DEFAULT NULL
+  `title` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `user-id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indizes der exportierten Tabellen
+-- Dumping data for table `video`
+--
+
+INSERT INTO `video` (`ID`, `title`, `description`, `user-id`) VALUES
+(1, 'test-title-1', 'test-desc-1', 0),
+(2, 'test-title-2', 'test-desc-2', 0);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `playlist`
+-- Indexes for table `playlist`
 --
 ALTER TABLE `playlist`
-  ADD PRIMARY KEY (`roomID`,`videoID`),
-  ADD KEY `videoID` (`videoID`),
-  ADD KEY `roomID` (`roomID`);
+  ADD PRIMARY KEY (`room_ID`,`video_ID`),
+  ADD KEY `videoID` (`video_ID`),
+  ADD KEY `roomID` (`room_ID`);
 
 --
--- Indizes für die Tabelle `room`
+-- Indexes for table `room`
 --
 ALTER TABLE `room`
   ADD PRIMARY KEY (`ID`),
@@ -102,56 +135,56 @@ ALTER TABLE `room`
   ADD KEY `ID_3` (`ID`);
 
 --
--- Indizes für die Tabelle `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `room-id` (`room-id`);
+  ADD KEY `room-id` (`room_id`);
 
 --
--- Indizes für die Tabelle `video`
+-- Indexes for table `video`
 --
 ALTER TABLE `video`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `room`
+-- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `user`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT für Tabelle `video`
---
-ALTER TABLE `video`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `playlist`
---
-ALTER TABLE `playlist`
-  ADD CONSTRAINT `FkRoomID` FOREIGN KEY (`roomID`) REFERENCES `room` (`ID`),
-  ADD CONSTRAINT `FkVideoID` FOREIGN KEY (`videoID`) REFERENCES `video` (`ID`);
-
---
--- Constraints der Tabelle `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `FkRoomIDFromUser` FOREIGN KEY (`room-id`) REFERENCES `room` (`ID`);
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `video`
+--
+ALTER TABLE `video`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `FkRoomID` FOREIGN KEY (`room_ID`) REFERENCES `room` (`ID`),
+  ADD CONSTRAINT `FkVideoID` FOREIGN KEY (`video_ID`) REFERENCES `video` (`ID`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FkRoomIDFromUser` FOREIGN KEY (`room_id`) REFERENCES `room` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
