ecc92b1c71554f4da6b788838a9676953923fe93
ArianMoser <arian_moser@yahoo.de>
2018-04-27 14:39:27 +0200

    Updated authentification process in the chat

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -30,77 +30,124 @@ export default class Chat extends Component {
     this.state = {
       chat: [],
       history: [],
+      hv: "",
       message: "",
       timestamp: "no timestamp yet",
       username: "default-username"
     };
 
+    // bind event handlers
     this._send = this._send.bind(this);
     this._messageUpdated = this._messageUpdated.bind(this);
     this._refreshChatText = this._refreshChatText.bind(this);
-
+    this._handleMessageSendCheck = this._handleMessageSendCheck.bind(this);
+    this.__authentificateOnServer = this._authentificateOnServer.bind(this);
     //  setInterval(this._refreshChatText, 1000);
   }
-
-  _send() {
-    //console.log("Kiebling");
-    var timeStamp = Math.floor(Date.now() / 1000);
-    socket.emit("sendMessage", {
-      content: this.state.message,
-      username: this.state.username,
-      timeStamp: timeStamp
-    });
-    this.setState({ message: "" });
-    this._refreshChatText();
-  }
-  _messageUpdated(e) {
-    this.setState({ message: e.target.value });
-  }
-
-  _refreshChatText() {
-    socket.on("sendMessageBack", message => {
-      if (message.length != 0) {
-        console.log(message.message.content);
-      }
-      console.log(message);
-      var chat = this.state.chat;
-      console.log(chat);
-      if (chat != [] && chat.length != "0") {
-        if (chat[chat.length-1].timeStamp != message.message.timeStamp){
-          chat.push({
-            username: message.message.username,
-            message: message.message.content,
-            timeStamp: message.message.timeStamp
-          });
-          this.setState({ chat: chat });
-        }
-      } else {
-        chat.push({
-          username: message.message.username,
-          message: message.message.content,
-          timeStamp: message.message.timeStamp
-        });
-        this.setState({ chat: chat });
-      }
-
-      //  console.log("chatText:" );
-    });
-  }
+  //-------------------------functions of react----------------------------//
 
   componentDidMount() {
     // Register user to the chat on the server
     //socket.emit("registerToChat", {username: this.state.username, roomId: this.props.hv});
     //console.log("roomId:");
     //console.log(this.props.hv);
+    this._authentificateOnServer();
   }
 
   componentWillMount() {
     var username = checksession();
     if (username != "ErrorTokenFalse") {
-      this.state.username = username;
+      this.setState({
+        username: username
+      });
     } else {
-      this.state.username = "undefined user";
+      this.setState({
+        username: "undefined"
+      });
     }
+
+    this.props.hv != undefined && this.props.hv != ""
+      ? this.setState({ hv: this.props.hv })
+      : this.setState({ hv: "" });
+  }
+
+  //----------------------------event handlers---------------------------//
+
+  _messageUpdated(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  _handleMessageSendCheck(event) {
+    if (event.charCode == 13 && this.state.message != "") {
+      this._send();
+    }
+  }
+  //----------------------functions------------------------------//
+
+  _authentificateOnServer() {
+    console.log("Authentificate on Server");
+    socket.emit("authentificate", {
+      hashedValue: this.state.hv,
+      username: this.state.username
+    });
+    this._refreshChatText();
+  }
+
+  _send() {
+    if (this.state.message != "") {
+      var timeStamp = Math.floor(Date.now() / 1000);
+      socket.emit("sendMessage", {
+        content: this.state.message,
+        username: this.state.username,
+        timeStamp: timeStamp
+      });
+      this.setState({ message: "" });
+      this._refreshChatText();
+    }
+  }
+
+  _refreshChatText() {
+    socket.on("sendMessageBack", message => {
+      if (message.length != 0) {
+        console.log(message.message.content);
+      } // end of if
+      console.log(message);
+      var chat = this.state.chat;
+      console.log(chat);
+      var beautifulTime = this.getTime(message.message.timeStamp);
+      if (chat != [] && chat.length != "0") {
+        if (chat[chat.length - 1].timeStamp != message.message.timeStamp) {
+          chat.push({
+            beautifulTime: beautifulTime,
+            message: message.message.content,
+            username: message.message.username,
+            timeStamp: message.message.timeStamp
+          });
+          this.setState({ chat: chat });
+        }
+      } else {
+        chat.push({
+          beautifulTime: beautifulTime,
+          message: message.message.content,
+          username: message.message.username,
+          timeStamp: message.message.timeStamp
+        });
+        this.setState({ chat: chat });
+      }
+    });
+  }
+
+  getTime(timeStamp) {
+    var date = new Date(timeStamp * 1000);
+    var hours = date.getHours();
+    hours = hours < 10 ? "0" + hours : hours;
+    var minutes = date.getMinutes();
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var seconds = date.getSeconds();
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    var beautifulTime = hours + ":" + minutes + ":" + seconds;
+    console.log(beautifulTime);
+    return beautifulTime;
   }
 
   //--------------------------------Render----------------------------------//
@@ -109,18 +156,35 @@ export default class Chat extends Component {
 
     if (chatTextElement != [] && chatTextElement != undefined) {
       var chat = this.state.chat;
+      var username = this.state.username;
       chatTextElement = chat.map(function(chatElement, index) {
-        return (
-          <List.Item>
-            <List.Content key={index}>
-              {" "}
-              <List.Header>
-                {chatElement.username} ({chatElement.timeStamp}) :
-              </List.Header>{" "}
-              {chatElement.message}{" "}
-            </List.Content>
-          </List.Item>
-        );
+        if (chatElement.username != username) {
+          console.log("unequal");
+          return (
+            <List.Item>
+              <List.Content key={index}>
+                {" "}
+                <List.Header>
+                  {chatElement.username} ({chatElement.beautifulTime}) :
+                </List.Header>{" "}
+                {chatElement.message}{" "}
+              </List.Content>
+            </List.Item>
+          );
+        } else {
+          console.log("equal");
+          return (
+            <List.Item>
+              <List.Content key={index} style={{ textAlign: "right" }}>
+                {" "}
+                <List.Header>
+                  {chatElement.username} ({chatElement.beautifulTime}) :
+                </List.Header>{" "}
+                {chatElement.message}{" "}
+              </List.Content>
+            </List.Item>
+          );
+        } //end of else
       });
     }
 
@@ -144,6 +208,7 @@ export default class Chat extends Component {
               placeholder="Chat..."
               value={this.state.message}
               onChange={this._messageUpdated}
+              onKeyPress={this._handleMessageSendCheck}
             />
             <Button onClick={this._send}>
               Send<Icon name="right arrow" />
