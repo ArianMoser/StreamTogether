5ba67601cc74ff1c21bde1671c92241935f73986
ArianMoser <arian_moser@yahoo.de>
2018-04-29 15:20:51 +0200

    Updated sql scheme

--- database/streamtogether.sql
+++ database/streamtogether.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 27. Apr 2018 um 15:59
--- Server-Version: 10.1.31-MariaDB
--- PHP-Version: 5.6.34
+-- Generation Time: Apr 29, 2018 at 03:18 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `streamtogether`
+-- Database: `streamtogether`
 --
 CREATE DATABASE IF NOT EXISTS `streamtogether` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `streamtogether`;
@@ -27,29 +27,21 @@ USE `streamtogether`;
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `playlist`
+-- Table structure for table `playlist`
 --
 
 CREATE TABLE `playlist` (
   `room_ID` int(11) NOT NULL,
   `video_ID` int(11) NOT NULL,
-  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `started` bigint(13) NOT NULL DEFAULT '0',
   `Upvotes` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `playlist`
---
-
-INSERT INTO `playlist` (`room_ID`, `video_ID`, `Timestamp`, `Upvotes`) VALUES
-(32, 4, '2018-04-26 15:57:51', 1),
-(32, 5, '2018-04-26 15:58:03', 1),
-(32, 6, '2018-04-26 15:58:08', 1);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `room`
+-- Table structure for table `room`
 --
 
 CREATE TABLE `room` (
@@ -63,21 +55,10 @@ CREATE TABLE `room` (
   `CreationDate` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Daten für Tabelle `room`
---
-
-INSERT INTO `room` (`ID`, `hashedValue`, `title`, `description`, `password`, `thumbnail`, `creator`, `CreationDate`) VALUES
-(0, '', NULL, NULL, '', 'room_default.png', 11, '0000-00-00 00:00:00.000000'),
-(32, '$2a$11$7FYUeQzV0T66GS7S7YLRt.DKEtdKeiaL816f93xihU.4wsY/.dLnG', 'Hadawd2', 'gasdfawda', '', 'room_default.png', 11, '2018-04-26 15:57:22.774853'),
-(34, '$2a$11$ehlO6SRvKuFoXAjD0dU3M.8krIB9g0wZJ1KtCvBdxjTEtEfapc9jy', '1', '1', '', 'room_default.png', 11, '2018-04-27 13:42:23.841649'),
-(35, '$2a$11$cZo/8uBG3xeaBw65MfHCjupb1WbwWz4Jq63fiIWDZzwKnwaSDGpyu', '11', '11', '', 'room_default.png', 11, '2018-04-27 13:43:25.576970'),
-(36, '$2a$11$LOkQX4/OodXFnlx1YibeFua1FFs46rGI/dWs1YWP3c5gSKqxli61C', '111', '', '', 'room_default.png', 11, '2018-04-27 13:52:05.532699');
-
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -85,11 +66,11 @@ CREATE TABLE `user` (
   `username` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `email` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `password` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `current_room_id` int(11) DEFAULT '0'
+  `current_room_id` int(11) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for all users ';
 
 --
--- Daten für Tabelle `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`ID`, `username`, `email`, `password`, `current_room_id`) VALUES
@@ -99,14 +80,13 @@ INSERT INTO `user` (`ID`, `username`, `email`, `password`, `current_room_id`) VA
 (6, '12', '12', '$2a$11$EQAnKaSQYwaFhmzw1qioze97S/g6mS22.AoFZNn7j.TQbpFiuvd.K', NULL),
 (7, '13', '13', '$2a$11$OvRuUUza9I/VV35Ircr6cuKqrUfWtFkf4TS29hon/Kj1dpzZiLElW', NULL),
 (8, '123', 'test@test.de', '$2a$11$Sj/6h2UEQy5ljwVkqtZahOev27XrKN/eulSmoUfw2BSJuhWXYrlO2', NULL),
-(11, '1', '1@1.de', '$2a$11$OrcNLHTZ4JZ2HZVTbX9/xOW1kno.5RMBrQ3T1Q0TrVpOK3OhkDviO', 36),
-(13, 'peter', 'd@d.de', '$2a$11$MKyfPguQsBsKgkIf/n11C..TMre4YsRFJ0ZuNgTMKxXkrpBcxdk0G', 0),
-(14, 'll', 'll@ll.de', '$2a$11$mn/MDm7iTpIGQhlw6qWbC.BXRP2Bi/aZQU.04fjd3u6bujXJEfXRS', 0);
+(11, '1', '1@1.de', '$2a$11$OrcNLHTZ4JZ2HZVTbX9/xOW1kno.5RMBrQ3T1Q0TrVpOK3OhkDviO', NULL),
+(12, 'PenisPenis', 'penis@penis.de', '$2a$11$PLmr1p3MnCmi.KX0cLrgM.gHS0NofIpEtthqBleraQxQ3Uxgy4Mya', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `video`
+-- Table structure for table `video`
 --
 
 CREATE TABLE `video` (
@@ -120,21 +100,11 @@ CREATE TABLE `video` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Daten für Tabelle `video`
---
-
-INSERT INTO `video` (`ID`, `youtube_id`, `title`, `description`, `thumbnail_url`, `channel_id`, `channel_name`) VALUES
-(4, 'aa_mgxO5v-A', 'DJ Lari Luke beim Dosenbeatz 2016', 'Larissa Rieß aka. Lari Luke beim Dosenbeatz 2016 in Köln Dj Booking: ilke@der-bomber-der-herzen.de.', 'https://i.ytimg.com/vi/aa_mgxO5v-A/default.jpg', 'UCgu3zpNiGmnEvQG_erWcsHg', 'Larissa White'),
-(5, 'Oe3FG4EOgyU', 'Schnappi Das Kleine Krokodil', 'Schnappi The Small Crocodile.', 'https://i.ytimg.com/vi/Oe3FG4EOgyU/default.jpg', 'UCgfF-gIiVz_GSCrIRpzZdmg', 'Sentinel79'),
-(6, '7t3Re2VIbHE', 'Ed Sheeran - Bibia Be Ye Ye', 'Out Now: https://atlanti.cr/yt-album Subscribe to Ed\'s channel: http://bit.ly/SubscribeToEdSheeran Follow Ed on... Facebook: http://www.facebook.com/EdSheeranMusic Twitter: http://twitter.com/...', 'https://i.ytimg.com/vi/7t3Re2VIbHE/default.jpg', 'UC0C-w0YjGpqDXGB8IHb662A', 'Ed Sheeran'),
-(7, 'T2JVCoLZ6r0', 'DASD', 'dsad.', 'https://i.ytimg.com/vi/T2JVCoLZ6r0/default.jpg', 'UCyt65rRaLFWm5Hfw5ghgmUA', 'fuckingfranko');
-
---
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `playlist`
+-- Indexes for table `playlist`
 --
 ALTER TABLE `playlist`
   ADD PRIMARY KEY (`room_ID`,`video_ID`),
@@ -142,79 +112,75 @@ ALTER TABLE `playlist`
   ADD KEY `roomID` (`room_ID`);
 
 --
--- Indizes für die Tabelle `room`
+-- Indexes for table `room`
 --
 ALTER TABLE `room`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `creator` (`creator`);
 
 --
--- Indizes für die Tabelle `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `room-id` (`current_room_id`);
 
 --
--- Indizes für die Tabelle `video`
+-- Indexes for table `video`
 --
 ALTER TABLE `video`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `room`
+-- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT für Tabelle `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT für Tabelle `video`
+-- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- Constraints der exportierten Tabellen
+-- Constraints for dumped tables
 --
 
 --
--- Constraints der Tabelle `playlist`
+-- Constraints for table `playlist`
 --
 ALTER TABLE `playlist`
   ADD CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`room_ID`) REFERENCES `room` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `playlist_ibfk_2` FOREIGN KEY (`video_ID`) REFERENCES `video` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `room`
+-- Constraints for table `room`
 --
 ALTER TABLE `room`
   ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`current_room_id`) REFERENCES `room` (`ID`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 DELIMITER $$
 --
--- Ereignisse
+-- Events
 --
-CREATE DEFINER=`root`@`localhost` EVENT `dropRoom35` ON SCHEDULE AT '2018-04-27 16:43:25' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM room WHERE room.id=35$$
-
-CREATE DEFINER=`root`@`localhost` EVENT `dropRoom36` ON SCHEDULE AT '2018-04-27 16:52:05' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM room WHERE room.id=36$$
-
-CREATE DEFINER=`root`@`localhost` EVENT `dropRoom34` ON SCHEDULE AT '2018-04-27 16:42:23' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM room WHERE room.id=34$$
+CREATE DEFINER=`root`@`localhost` EVENT `dropRoom3` ON SCHEDULE AT '2018-04-29 16:16:11' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM room WHERE room.id=3$$
 
 DELIMITER ;
 COMMIT;
